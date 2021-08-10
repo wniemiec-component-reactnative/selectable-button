9ae16fd2ed6472edd38911ecbc89788103deef59
William Niemiec <williamniemiec@hotmail.com>
2021-08-10 09:52:52 -0300

    Doc

--- src/SelectableButton/index.js
+++ src/SelectableButton/index.js
@@ -24,29 +24,44 @@ import styles from './styles';
  * @param   {string} [size='regular'] Button size
  * @param   {boolean} [full=false] Indicates whether the width should be 100%
  */
-export default function SelectableButton({ title, onPress, selected, bgColor='#01786F', fgColor='#FFFFFF', size='regular', full=false }) {
+export default function SelectableButton({
+  title, 
+  onPress, 
+  selected, 
+  bgColor='#01786F', 
+  fgColor='#FFFFFF', 
+  size='regular', 
+  full=false 
+}) {
 
-  const factor = buildFactor(size.toLowerCase().trim());
+  const factor = buildFactor(size);
 
   return (
     <TouchableOpacity 
       style={buildButtonStyle(factor, bgColor, fgColor, selected, full)} 
       onPress={onPress}
     >
-      <Text style={buildButtonTextStyle(factor, fgColor)}>{title}</Text>
+      <Text style={buildButtonTextStyle(factor, fgColor)}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
 
 function buildFactor(size) {
+  const normalizedSize = normalizeSize(size);
   let factor = 1.3;
 
-  if (size === 'small')
+  if (normalizedSize === 'small')
     factor = 1.0;
-  else if (size === 'big')
+  else if (normalizedSize === 'big')
     factor = 2.0;
 
   return factor;
+}
+
+function normalizeSize(size) {
+  return size.toLowerCase().trim();
 }
 
 function buildButtonStyle(factor, bgColor, fgColor, selected, full) {
